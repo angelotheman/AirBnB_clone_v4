292898711c5da888e4b91058983ab90477e721a3
Kwabena Twumasi <ktwum721@gmail.com>
2024-04-07 22:56:05 +0000

    Added method for requesting api

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,7 +16,7 @@ $(function () {
   });
 
   $.get('http://0.0.0.0:5001/api/v1/status', function (data) {
-    if (datal.status === 'OK') {
+    if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
